62398e88dc9a87054a0cfdcacaffb7446cd1bfa0
Walcker Gomes <walckergomes@gmail.com>
2020-03-05 22:06:47 +0000

    Mudanças

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
 #include "aluno.h"
 #include "turma.h"
+using namespace std;
 
-int main(){
-
-	Aluno r;
-	r.setMatricula(10);
-	r.setIdade(50);
-  r.setNome("walcker")
-
-	std::cout << "Matricula = " << r.getMatricula() << ", Idade = " << r.getIdade() << "Nome: = " << r.getNome;
-
-  Turma s;
-	s.setCodigo(101);
-	s.setDescricao("EDB1");
-  s.setCapacidade("walcker")
-
-	std::cout << "Codigo = " << s.getMatricula() << ", Descricao = " << s.getIdade() << "Capacidade: = " << s.getNome;
-
-	return 0;
+int main() {
+  Aluno eu;
+  eu.setMatricula("123\n");
+  eu.setIdade(36);
+  eu.setNome("emerson");
+  std::cout << "matricula = " << eu.getMatricula() << "idade = " << eu.getIdade() << endl << "nome = " << eu.getNome() << std::endl;
+return 0;
 }
